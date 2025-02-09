7bd8c8ce7b880c421f73397cb4a9f871b1099c98
Kirillmlk <kostakov.kiirill@yandex.by>
2025-02-09 17:05:43 +0300

    Chat list page

---
+++ resources/js/Layouts/Main.vue
@@ -0,0 +1,27 @@
+<script>
+
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    name: "Main",
+
+    components: {
+        Link
+    },
+}
+</script>
+
+<template>
+    <div class="p-4 w-3/4 mx-auto">
+        <div>
+            <div>
+                <Link :href="route('chats.index')">Chats</Link>
+            </div>
+        </div>
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex">
+        <div class="w-1/2 border border-gray-600">
+            lorem ipsum dolor
+        </div>
+        <div class="w-1/2 border border-gray-600">
+            lorem ipsum dolor lorem ipsum dolor lorem ipsum dolorlorem ipsum dolorlorem ipsum dolorlorem ipsum dolorlorem ipsum dolorlorem ipsum dolorlorem ipsum dolorlorem ipsum dolorlorem ipsum dolor
+        </div>
+    </div>
+</template>
+
+<script>
+import Main from "@/Layouts/Main.vue";
+
+export default {
+    name: "Index",
+
+    layout: Main,
+}
+</script>
+
+<style scoped>
+
+</style>
