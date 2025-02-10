bef7e369e10cde3e8b04a6e8fd5ddad8cdd0dfc0
Kirillmlk <kostakov.kiirill@yandex.by>
2025-02-10 22:33:56 +0300

    Displaying the chat list

--- resources/js/Pages/Chat/Index.vue
+++ resources/js/Pages/Chat/Index.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="flex">
         <div class="w-1/2 p-4 mr-4 bg-white border border-gray-200">
-            lorem ipsum dolor
+            <h3 class="text-gray-700 mb-4 text-lg">Chats</h3>
+            <div v-if="chats" class="text-gray-700 mb-4 text-lg">
+                <div v-for="chat in chats" class="flex items-center pb-4 mb-4 border-b border-gray-300">
+                    <Link :href="route('chats.show', chat.id)" class="flex">
+                        <p class="mr-2">{{ chat.id }}</p>
+                        <p class="mr-2">{{ chat.title ?? 'Your chat' }}</p>
+                    </Link>
+                </div>
+            </div>
         </div>
         <div class="w-1/2 p-4 bg-white border border-gray-200">
-            <h3>Users</h3>
+            <h3 class="text-gray-700 mb-4 text-lg">Users</h3>
             <div v-if="users" class="text-gray-700 mb-4 text-lg">
                 <div v-for="user in users" class="flex items-center pb-4 mb-4 border-b border-gray-300">
-                    <p class="mr-2">{{ user.id}}</p>
-                    <p class="mr-4">{{ user.name}}</p>
-                    <a @click.prevent="store(user.id)" class="inline-block bg-sky-400 text-white text-xs px-3 py-2 rounded-lg" href="#">Message</a>
+                    <p class="mr-2">{{ user.id }}</p>
+                    <p class="mr-4">{{ user.name }}</p>
+                    <a @click.prevent="store(user.id)"
+                       class="inline-block bg-sky-400 text-white text-xs px-3 py-2 rounded-lg" href="#">Message</a>
                 </div>
             </div>
         </div>
@@ -17,14 +26,20 @@
 </template>
 
 <script>
+import {Link} from "@inertiajs/vue3";
 import Main from "@/Layouts/Main.vue";
 
 export default {
     name: "Index",
 
     props: [
-      'users'
+        'users',
+        'chats'
     ],
+
+    components: {
+        Link,
+    },
 
     layout: Main,
 
